b3e7195cd085f71630a3a528a8137de8c99ef768
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-28 14:29:14 +0000

    npm packages helmet cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
 const app = express()
 const port = process.env.PORT || 3000
 const router = require('./router')
+
+app.use(helmet())
+app.use(cors())
 
 app.use(express.json())
 app.use(router)
