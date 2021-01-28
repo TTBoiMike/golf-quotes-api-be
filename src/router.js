1922c0099977406c7c53c7d7c02beeaad16e1b19
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-28 18:26:01 +0000

    update random tag path

--- src/router.js
+++ src/router.js
@@ -6,6 +6,6 @@ router.get('/quotes', quotes.index)
 router.get('/quotes/id/:id', quotes.quoteById)
 router.get('/quotes/tag/:tag', quotes.quoteByTag)
 router.get('/quotes/random', quotes.random)
-router.get('/quotes/random/:tag', quotes.random)
+router.get('/quotes/random/tag/:tag', quotes.random)
 
 module.exports= router
