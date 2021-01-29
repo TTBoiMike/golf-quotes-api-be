683993dd253e7bcbade227c37eb67589e97280b9
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-29 22:32:17 +0000

    add limit param

--- src/quotesController.js
+++ src/quotesController.js
@@ -6,7 +6,12 @@ exports.random = (req, res) => {
     res.send(quotes[number])
 }
 exports.randomByTag = (req, res) => {
+    // console.log(req.query.limit)
     let filteredQuotes = quotes.filter(quote => quote.tag.includes(req.params.tag))
     let number = Math.floor(Math.random() * filteredQuotes.length)
-    res.send(filteredQuotes[number])
+    if(req.query.limit === 1) {
+        res.send(filteredQuotes[number])
+    } else {
+        res.send(filteredQuotes.slice(0, req.query.limit))
+    }
 }
