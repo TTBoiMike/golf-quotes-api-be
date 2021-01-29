2a598988ba93c014c907f3c7ff5c9d65e83abcf7
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-29 21:29:44 +0000

    random by tag

--- src/quotesController.js
+++ src/quotesController.js
@@ -1,37 +1,12 @@
 const createError = require('http-errors');
 const quotes = require('./quotes');
 
-exports.index = (req, res) => {
-    res.send(quotes)
-}
-exports.quoteById = (req, res, next) => {
-    const quotesById = quotes.filter(quote => quote.id == req.params.id)
-    console.log(quotesById.length)
-    if(quotesById.length == 0) {
-        return(next(createError(404, `No quote with tag: ${req.params.id}`)))
-    }
-    res.send(quotesById)
-}
-exports.quoteByTag = (req, res, next) => {
-    const filteredQuotes = []
-    quotes.forEach(quote => {
-        if(quote.tags.includes(req.params.tag)) {
-            filteredQuotes.push({name: quote.name, quote: quote.quote})
-        }
-    })
-    if(filteredQuotes.length == 0) {
-        return(next(createError(404, `No quote with tag: ${req.params.tag}`)))
-    } else {
-        res.send(filteredQuotes)
-    }
-}
 exports.random = (req, res) => {
     let number = Math.floor(Math.random() * quotes.length)
-    if(req.params.tag !== undefined) {
-        let filteredQuotes = quotes.filter(quote => quote.tags.includes(req.params.tag))
-        res.send(filteredQuotes[number-1])
-    } else {
-        console.log("random")
-        res.send(quotes[number])
-    }
+    res.send(quotes[number])
+}
+exports.randomByTag = (req, res) => {
+    let filteredQuotes = quotes.filter(quote => quote.tag.includes(req.params.tag))
+    let number = Math.floor(Math.random() * filteredQuotes.length)
+    res.send(filteredQuotes[number])
 }
--- src/router.js
+++ src/router.js
@@ -2,10 +2,7 @@ const express = require('express')
 const router = express.Router()
 const quotes = require('./quotesController')
 
-router.get('/quotes', quotes.index)
-router.get('/quotes/id/:id', quotes.quoteById)
-router.get('/quotes/tag/:tag', quotes.quoteByTag)
 router.get('/quotes/random', quotes.random)
-router.get('/quotes/random/tag/:tag', quotes.random)
+router.get('/quotes/random/tag/:tag', quotes.randomByTag)
 
 module.exports= router
