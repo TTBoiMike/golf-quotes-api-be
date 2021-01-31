92cd7f47196654cfcd668b0edeea98e2b83166fa
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-31 10:01:23 +0000

    remove limit

--- src/quotesController.js
+++ src/quotesController.js
@@ -3,15 +3,10 @@ const quotes = require('./quotes');
 
 exports.random = (req, res) => {
     let number = Math.floor(Math.random() * quotes.length)
-    res.send([quotes[number]])
+    res.send(quotes[number])
 }
 exports.randomByTag = (req, res) => {
-    // console.log(req.query.limit)
     let filteredQuotes = quotes.filter(quote => quote.tag.includes(req.params.tag))
     let number = Math.floor(Math.random() * filteredQuotes.length)
-    if(req.query.limit === 1) {
-        res.send(filteredQuotes[number])
-    } else {
-        res.send(filteredQuotes.slice(0, req.query.limit))
-    }
+    res.send(filteredQuotes[number])
 }
