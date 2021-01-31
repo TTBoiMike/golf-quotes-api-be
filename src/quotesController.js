99c40b41d4d9e41e4c1905ae322a7f929b8bc65e
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-31 10:47:41 +0000

    add name route

--- src/quotesController.js
+++ src/quotesController.js
@@ -1,12 +1,39 @@
 const createError = require('http-errors');
 const quotes = require('./quotes');
 
+// return all quotes
+// return a certain number of quotes
+exports.index = (req, res) => {
+     if(req.query.limit) {
+        res.send(quotes.slice(0, req.params.limit))
+    } else {
+        res.send(quotes)
+    }
+}
+// get a single random quote
 exports.random = (req, res) => {
     let number = Math.floor(Math.random() * quotes.length)
     res.send(quotes[number])
 }
+// get a single random quote by tag
+// get a certain number of quotes by tag
 exports.randomByTag = (req, res) => {
     let filteredQuotes = quotes.filter(quote => quote.tag.includes(req.params.tag))
     let number = Math.floor(Math.random() * filteredQuotes.length)
-    res.send(filteredQuotes[number])
+    if(req.query.limit) {
+        res.send(filteredQuotes.slice(0, req.query.limit))
+    } else {
+        res.send(filteredQuotes[number])
+    }
+}
+// get a random quote by player (surname)
+// get a certain number of quotes by surname
+exports.randomByName = (req, res) => {
+    let filteredQuotes = quotes.filter(quote => quote.name.surname.toLowerCase() == req.params.name)
+    let number = Math.floor(Math.random() * filteredQuotes.length)
+        if(req.query.limit) {
+        res.send(filteredQuotes.slice(0, req.query.limit))
+    } else {
+        res.send(filteredQuotes[number])
+    }
 }
--- src/router.js
+++ src/router.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 const quotes = require('./quotesController')
 
+// returns all quotes
+router.get('/quotes', quotes.index)
+// returns a random quote
 router.get('/quotes/random', quotes.random)
+// returns a random quote by tag or a set number by tag
 router.get('/quotes/random/tag/:tag', quotes.randomByTag)
+// returns a random quote by name or a set number by name
+router.get('/quotes/random/name/:name', quotes.randomByName)
 
 module.exports= router
