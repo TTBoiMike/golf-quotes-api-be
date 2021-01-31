e99a745873472ba60b69048c9054595262f56a90
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-31 14:17:21 +0000

    add params to routes

--- src/quotesController.js
+++ src/quotesController.js
@@ -1,22 +1,45 @@
 const createError = require('http-errors');
 const quotes = require('./quotes');
 
-// return all quotes
-// return a certain number of quotes
-exports.index = (req, res) => {
-     if(req.query.limit) {
-        res.send(quotes.slice(0, req.params.limit))
+exports.index = (req, res, next) => {
+
+    // /quotes/
+    // /quotes/?tag=funny
+    // /quotes/?name=palmer
+    // /quotes/?name=palmer&tag=funny
+
+
+    if(req.query.tag) {
+        let filteredByTag = quotes.filter(quote => quote.tag.includes(req.query.tag))
+        if(req.query.name) {
+            let filteredByNameAndTag = filteredByTag.filter(quote => quote.name.surname.toLowerCase() === req.query.name)
+            if(filteredByNameAndTag.length === 0) {
+                return (next(createError(404,`Error. No ${req.query.name} quotes tagged as ${req.query.tag}`)))
+            } else {
+            res.send(filteredByNameAndTag)
+            }
+        } else {
+            if(filteredByTag.length === 0) {
+                return (next(createError(404,`Error. No quotes tagged as ${req.query.tag}`)))
+            } else {
+                res.send(filteredByTag)
+            }
+        }
+    } else if(req.query.name) {
+        let filteredByName = quotes.filter(quote => quote.name.surname.toLowerCase() === req.query.name)
+        if(filteredByName.length === 0) {
+            return (next(createError(404,`Error. No quotes with the surname ${req.query.name}`)))
+        } else {
+            res.send(filteredByName)
+        }
     } else {
         res.send(quotes)
     }
 }
-// get a single random quote
 exports.random = (req, res) => {
     let number = Math.floor(Math.random() * quotes.length)
     res.send(quotes[number])
 }
-// get a single random quote by tag
-// get a certain number of quotes by tag
 exports.randomByTag = (req, res) => {
     let filteredQuotes = quotes.filter(quote => quote.tag.includes(req.params.tag))
     let number = Math.floor(Math.random() * filteredQuotes.length)
@@ -26,14 +49,17 @@ exports.randomByTag = (req, res) => {
         res.send(filteredQuotes[number])
     }
 }
-// get a random quote by player (surname)
-// get a certain number of quotes by surname
-exports.randomByName = (req, res) => {
-    let filteredQuotes = quotes.filter(quote => quote.name.surname.toLowerCase() == req.params.name)
-    let number = Math.floor(Math.random() * filteredQuotes.length)
-        if(req.query.limit) {
-        res.send(filteredQuotes.slice(0, req.query.limit))
+exports.randomByName = (req, res, next) => {
+    let filteredByName = quotes.filter(quote => quote.name.surname.toLowerCase() == req.params.name)
+    let number = Math.floor(Math.random() * filteredByName.length)
+    if(req.query.tag) {
+        let filteredByTag = filteredByName.filter(quote => quote.tag.includes(req.query.tag))
+        if(filteredByTag.length === 0) {
+            return (next(createError(404,`Error, No ${req.params.name} quotes tagged as ${req.query.tag}`)))
+        } else {
+            res.send(filteredByTag[number])
+        }
     } else {
-        res.send(filteredQuotes[number])
+    res.send(filteredByName[number])
     }
 }
